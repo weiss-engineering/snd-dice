98b3873e23fd6c6181f50a2f46ef788df1dc96d3
Michele Perrone <michele.perrone@weiss.ch>
2023-07-06 10:06:07 +0200

    Hardcoded mode capabilities for remaining devices: - DAC202 Maya edition - INT202 - INT203 - ADC2 - DAC2/Minerva - Vesta - AFI1

--- dice-weiss.c
+++ dice-weiss.c
@@ -13,18 +13,61 @@ struct dice_weiss_spec {
 	bool has_midi;
 };
 
+/* Weiss DAC202: 192kHz 2-channel DAC */
 static const struct dice_weiss_spec dac202 = {
 	.tx_pcm_chs = {{2, 2, 2}, {0, 0, 0} },
 	.rx_pcm_chs = {{2, 2, 2}, {0, 0, 0} },
 	.has_midi   = false
 };
 
+/* Weiss MAN301: 192kHz 2-channel music archive network player */
 static const struct dice_weiss_spec man301 = {
 	.tx_pcm_chs = {{2, 2, 2}, {0, 0, 0} },
 	.rx_pcm_chs = {{2, 2, 2}, {0, 0, 0} },
 	.has_midi   = false
 };
 
+/* Weiss INT202: 192kHz unidirectional 2-channel digital Firewire interface */
+static const struct dice_weiss_spec int202 = {
+	.tx_pcm_chs = {{2, 2, 2}, {0, 0, 0} },
+	.rx_pcm_chs = {{0, 0, 0}, {0, 0, 0} },
+	.has_midi   = false
+};
+
+/* Weiss INT203: 192kHz bidirectional 2-channel digital Firewire interface */
+static const struct dice_weiss_spec int203 = {
+	.tx_pcm_chs = {{2, 2, 2}, {0, 0, 0} },
+	.rx_pcm_chs = {{2, 2, 2}, {0, 0, 0} },
+	.has_midi   = false
+};
+
+/* Weiss ADC2: 192kHz A/D converter with microphone preamps and line inputs */
+static const struct dice_weiss_spec adc2 = {
+	.tx_pcm_chs = {{2, 2, 2}, {0, 0, 0} },
+	.rx_pcm_chs = {{2, 2, 2}, {0, 0, 0} },
+	.has_midi   = false
+};
+
+/* Weiss DAC2/Minerva: 192kHz 2-channel DAC */
+static const struct dice_weiss_spec dac2_minerva = {
+	.tx_pcm_chs = {{2, 2, 2}, {0, 0, 0} },
+	.rx_pcm_chs = {{2, 2, 2}, {0, 0, 0} },
+	.has_midi   = false
+};
+
+/* Weiss Vesta: 192kHz 2-channel Firewire to AES/EBU interface */
+static const struct dice_weiss_spec vesta = {
+	.tx_pcm_chs = {{2, 2, 2}, {0, 0, 0} },
+	.rx_pcm_chs = {{2, 2, 2}, {0, 0, 0} },
+	.has_midi   = false
+};
+
+/* Weiss AFI1: 192kHz 24-channel Firewire to ADAT or AES/EBU interface */
+static const struct dice_weiss_spec afi1 = {
+	.tx_pcm_chs = {{24, 16, 8}, {0, 0, 0} },
+	.rx_pcm_chs = {{24, 16, 8}, {0, 0, 0} },
+	.has_midi   = false
+};
 
 int snd_dice_detect_weiss_formats(struct snd_dice *dice)
 {
@@ -33,7 +76,14 @@ int snd_dice_detect_weiss_formats(struct snd_dice *dice)
 		const struct dice_weiss_spec *spec;
 	} *entry, entries[] = {
 		{0x000007, &dac202},
-		{0x00000b, &man301}
+		{0x000008, &dac202}, /* Maya edition: same audio I/O as DAC202 */
+		{0x000006, &int202},
+		{0x00000a, &int203},
+		{0x00000b, &man301},
+		{0x000001, &adc2},
+		{0x000003, &dac2_minerva},
+		{0x000002, &vesta},
+		{0x000004, &afi1},
 	};
 	struct fw_csr_iterator it;
 	int key, val, model_id;
